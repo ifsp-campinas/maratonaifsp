a9f44a5ee65d9a59599c96642d4f32340dfc8704
ivycabral123 <almeidaderafael@gmail.com>
2020-12-07 10:41:04 -0300

    primeira versão do projeto

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Videos from './components/videos';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Site maratona de programação IFSP Campinas</h1>
+      <Videos />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/videos.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const videos = () =>{
+	return(
+		<>
+			<h2>Videos</h2>
+		</>
+		);
+}
+
+export default videos;
