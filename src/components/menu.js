5871b226a25807ebbe58acf659e35b3c3b0585d3
Rafael1302 <almeidaderafael@gmail.com>
2021-03-09 15:27:53 -0300

    alteração no menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 const menu = () =>{
 	return(
-		<>
-			<img src="../src/logoifsp.png"></img>
-			<a href="#">Section 1 </a>
-			<a href="#">Section 2 </a>
-			<a href="#">Section 3 </a>
-		</>
+		<div>
+			<h2>Instituto Federal de São Paulo</h2>
+			<h1>Câmpus Campinas</h1>
+			<a href="https://portal.cmp.ifsp.edu.br/">Section 1 </a>
+			<a href="https://portal.cmp.ifsp.edu.br/">Section 2 </a>
+			<a href="https://portal.cmp.ifsp.edu.br/">Section 3 </a>
+		</div>
 		);
 }
 
